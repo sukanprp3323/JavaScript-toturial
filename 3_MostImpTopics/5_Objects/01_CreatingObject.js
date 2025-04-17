b1b3ad4fed35794a95a35dcf269660b18aba3f1e
Sukan Prajapati <sukanprajapati3323@gmail.com>
2025-04-17 22:40:04 +0530

    initial commit

---
+++ 3_MostImpTopics/5_Objects/01_CreatingObject.js
@@ -0,0 +1,15 @@
+//! Many ways to create Object
+//? 1. Object literals : [Key:value]
+
+const Product = {
+    Id: 1,
+    Pname: "laptop",
+}
+
+const Product1 = {
+    Id: 2,
+    Pname: "Mobile",
+    greeting : function (){
+        console.log("welcome to Object");
+    },
+}
--- 3_MostImpTopics/5_Objects/01_objectToturial.js
+++
@@ -1,7 +0,0 @@
-//! Many ways to create Object
-//? 1. Object literals : [Key:value]
-
-const Product = {
-    Id: 1,
-    Pname: "laptop",
-}
